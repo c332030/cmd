e15961737e7bbbb3555508d613e8a4552d055998
c332030 <c332030@gmail.com>
2023-01-23 16:58:59 +0800

    feat: link-dir.bat

--- script/_link.bat
+++ script/_link.bat
@@ -6,7 +6,7 @@ setlocal
 
 set current_path=%~dp0
 
-call %current_path%\link-file.bat link-dir-prompt.bat %current_path% %windir%
+call %current_path%\link-file.bat link-dir.bat %current_path% %windir%
 
 call %current_path%\link-file.bat link-file.bat %current_path% %windir%
 
